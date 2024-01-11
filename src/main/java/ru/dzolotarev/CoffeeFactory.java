9ef236d9809d7f9caf06ca118fea0e77155ca023
Denis Zolotarev <judaeus@gmail.com>
2024-01-11 21:47:13 +0200

    * add factory method

---
+++ src/main/java/ru/dzolotarev/CoffeeFactory.java
@@ -0,0 +1,21 @@
+package ru.dzolotarev;
+
+/**
+ * @author Denis Zolotarev
+ */
+public class CoffeeFactory {
+
+    public static CoffeeInstructions getCOffeeInstructions(String coffeeName) {
+        if (coffeeName == null) {
+            return null;
+        }
+
+        CoffeeInstructions instructions = null;
+        if (coffeeName.equalsIgnoreCase("Эспрессо")) {
+            instructions = new EspressoInstructions();
+        } else if (coffeeName.equalsIgnoreCase("Латте")) {
+            instructions = new LatteInstructions();
+        }
+        return instructions;
+    }
+}
