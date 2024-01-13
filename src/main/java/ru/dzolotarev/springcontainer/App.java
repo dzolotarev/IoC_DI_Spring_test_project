a7540bc78f1735b7719ed3af3610a3481fd5d6b0
Denis Zolotarev <judaeus@gmail.com>
2024-01-13 21:51:21 +0200

    * BeanFactory & XML config example

--- src/main/java/ru/dzolotarev/springcontainer/App.java
+++ src/main/java/ru/dzolotarev/springcontainer/App.java
@@ -1,6 +1,8 @@
 package ru.dzolotarev.springcontainer;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author Denis Zolotarev
@@ -9,8 +11,30 @@ public class App {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        beanFactory.registerSingleton("myStr", new String("Hello there!"));
-        String bean = (String) beanFactory.getBean("myStr");
-        System.out.printf(bean);
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("applicationContext.xml"));
+
+        RestaurantService restaurantService = beanFactory.getBean(RestaurantService.class);
+        restaurantService.cook("макароны");
+    }
+
+    static class RestaurantService {
+
+        private FastCook fastCook;
+
+        public RestaurantService(FastCook fastCook) {
+            this.fastCook = fastCook;
+        }
+
+        public void cook(String dish) {
+            fastCook.cook(dish);
+        }
+    }
+
+    static class FastCook {
+
+        public void cook(String dish) {
+            System.out.println("Cooking " + dish);
+        }
     }
 }
