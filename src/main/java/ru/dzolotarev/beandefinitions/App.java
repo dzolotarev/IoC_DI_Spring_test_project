95285525d31c12eea921e4728a1caa4aaee6e66c
Denis Zolotarev <judaeus@gmail.com>
2024-01-14 23:39:00 +0200

    * add Bean Definitions via Java config

--- src/main/java/ru/dzolotarev/beandefinitions/App.java
+++ src/main/java/ru/dzolotarev/beandefinitions/App.java
@@ -3,11 +3,15 @@ package ru.dzolotarev.beandefinitions;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * @author Denis Zolotarev
  */
+
+@Configuration
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext =
@@ -17,5 +21,15 @@ public class App {
                 ((GenericApplicationContext) applicationContext).getBeanDefinition("restaurantService");
         System.out.println(beanDefinition);
         System.out.println(beanDefinition.getClass());
+    }
+
+    @Bean
+    public  RestaurantService restaurantService() {
+        return new RestaurantService(fastCook());
+    }
+
+    @Bean
+    public FastCook fastCook() {
+        return new FastCook();
     }
 }
